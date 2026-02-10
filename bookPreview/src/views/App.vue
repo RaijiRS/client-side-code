8e19f87fc721a3ba75d036c2ed5268c4337de936
rsacaza <rsacaza@eagles.bridgewater.edu>
2026-02-09 19:49:43 -0500

    bookPreview

---
+++ bookPreview/src/views/App.vue
@@ -0,0 +1,39 @@
+<script setup>
+import Book from '@/components/Book.vue';
+import Preview from '@/components/Preview.vue';
+import { ref, watch } from 'vue';
+
+
+const bookData = ref({
+    title: "",
+    author: "",
+    types: [],
+    datePublished:"" ,
+    description:"",
+    isBookValid: false
+})
+
+
+
+
+</script>
+
+<template>
+  <div class="container">
+    <Book v-model="bookData"/>
+    <Preview :book = "bookData"/>
+  </div>
+    
+</template>
+
+<style scoped>
+  .container{
+    display: flex;
+    justify-content: center;
+    gap: 200px;
+    
+    
+  }
+
+  
+</style>
